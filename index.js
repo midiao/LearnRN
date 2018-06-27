38ad87bcff12c8976a09d1b947949c56305a34ec
CY <wzdxc123@163.com>
2018-06-27 19:10:50 +0800

    增加登录页面和未完成的等待页面。

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
-import { AppRegistry } from 'react-native';
+import React, { Component } from 'react';
+import {
+    AppRegistry, BackHandler, Platform
+} from 'react-native';
 import LoginLeaf from './LoginLeaf';
-
-AppRegistry.registerComponent('LearnRN', () => LoginLeaf);
+// import WaitingLeaf from './WaitingLeaf';
+export default class NaviModule extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentScene: 'Login',
+            phoneNumber: '',
+            userPW: ''
+        };
+        this.handleBackSignal = this.handleBackSignal.bind(this);
+        this.onLoginPressed = this.onLoginPressed.bind(this);
+    }
+    onLoginPressed(aNumber, aPW) {
+        this.setState({
+            currentScene: 'Waiting',
+            phoneNumber: aNumber,
+            userPW: aPW
+        });
+    }
+    render() {
+        if (this.state.currentScene === 'Login') {
+            return <LoginLeaf onLoginPressed = {this.onLoginPressed} />;
+        } else return (
+            <WaitingLeaf phoneNumber = {this.state.phoneNumber}
+                onGobackPressed = {this.handleBackSignal}
+                userPW = {this.state.userPW} />
+        )
+    }
+    handleBackSignal() {
+        if (this.state.currentScene === 'Waiting') {
+            this.setState({
+                currentScene: 'Login'
+            });
+            return true;
+        }
+        return false;
+    }
+    componentDidMount() {
+        if (Platform.OS === "android") {
+            BackHandler.addEventListener('hardwareBackPress', this.handleBackSignal);
+        }
+    }
+    componentWillUnMount() {
+        if(Platform.OS === "android") {
+            BackHandler.removeEventListener('hardwareBackPress', this.handleBackSignal);
+        }
+    }
+}
+AppRegistry.registerComponent('LearnRN', () => NaviModule);
